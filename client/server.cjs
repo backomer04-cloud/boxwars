f4ab3d199a687bb38901c422e5ab47f6f8df53d9
backomer04-cloud <backomer04@gmail.com>
2026-09-04 02:10:55 +0300

    yenilen

--- client/server.cjs
+++ client/server.cjs
@@ -25,6 +25,9 @@ app.get("/", (req, res) => {
     res.send("BOX WARS SUNUCUSU ÇALIŞIYOR 🔥 (Supabase Sync Mode)");
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // --- SOCKET.IO OYUN İLETİŞİM RÖLESİ ---
 io.on("connection", (socket) => {
